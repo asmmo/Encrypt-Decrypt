27b82aac433c9b5f26f7732e3091335d5c089d28
Ammar Sayed Mohammed Mohammed <ammarsayed99ph@gmail.com>
2020-07-08 05:08:04 +0200

    Add files via upload

---
+++ Encrypt-Decrypt/Encrypt-Decrypt/Encrypt-DecryptDlg.h
@@ -0,0 +1,34 @@
+
+// Encrypt-DecryptDlg.h : header file
+//
+
+#pragma once
+
+
+// CEncryptDecryptDlg dialog
+class CEncryptDecryptDlg : public CDialogEx
+{
+// Construction
+public:
+	CEncryptDecryptDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ENCRYPTDECRYPT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
